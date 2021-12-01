0507eb1328b32c448cf5814a73482c19e853a007
DiegoFlamenco123 <00221020@uca.edu.sv>
2021-12-01 13:42:59 -0600

    commitFinal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/login";
 import Main from "./Pages/Main";
 import Posts from "./Pages/Posts";
